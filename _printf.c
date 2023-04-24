f064b26325d18364cd5f084ef0d93e58d5eabaac
grace-rono <ronograce04@gmail.com>
2023-04-24 04:07:03 -0700

    Write a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,66 +1,61 @@
-#include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - function that produces output like real printf
+ *
+ * @format:  is a character string
+ * Return: c
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+	va_list osarg;
+	int sum = 0;
+	int i;
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	va_start(osarg, format);
+	while (*format != '\0')
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			format++;
+			while (*format)
+			{
+				if ('c')
+				{
+					char character = va_arg(osarg, int);
+
+					write(STDOUT_FILENO, &character, 1);
+					sum++;
+				}
+				else ('%')
+				{
+					write(STDOUT_FILENO, "%", 1);
+					sum++;
+				}
+
+				else ('s')
+				{
+					char *string = va_arg(osarg, char*);
+
+					i = 0;
+					while (string[i] != '\0')
+					{
+					write(STDOUT_FILENO, &string[i], 1);
+					i++;
+					sum++;
+				}
+		}
+		}
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
+			write(1, format, 1);
+			sum++;
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+		format++;
+	}
+	va_end(osarg);
+	return (sum);
 }
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-

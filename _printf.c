94908291333513f86465a49df20cd19389f0b576
grace-rono <ronograce04@gmail.com>
2023-04-25 03:15:15 -0700

    Creating Printf

--- _printf.c
+++ _printf.c
@@ -1,61 +1,69 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - function that produces output like real printf
- *
- * @format:  is a character string
- * Return: c
+ * _printf -  function that produces output according to a format.
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	va_list osarg;
-	int sum = 0;
-	int i;
+	int j, printed = 0, printed_chars = 0;
 
-	va_start(osarg, format);
-	while (*format != '\0')
+	int flags, width, precision, size, buff_ind = 0;
+
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (j = 0; format && format[j] != '\0'; j++)
 	{
-		if (*format == '%')
+		if (format[j] != '%')
 		{
-			format++;
-			while (*format)
-			{
-				if ('c')
-				{
-					char character = va_arg(osarg, int);
-
-					write(STDOUT_FILENO, &character, 1);
-					sum++;
-				}
-				else ('%')
-				{
-					write(STDOUT_FILENO, "%", 1);
-					sum++;
-				}
-
-				else ('s')
-				{
-					char *string = va_arg(osarg, char*);
-
-					i = 0;
-					while (string[i] != '\0')
-					{
-					write(STDOUT_FILENO, &string[i], 1);
-					i++;
-					sum++;
-				}
-		}
-		}
+			buffer[buff_ind++] = format[j];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			write(1, format, 1);
-			sum++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &j);
+			width = get_width(format, &j, list);
+			precision = get_precision(format, &j, list);
+			size = get_size(format, &j);
+			++j;
+			printed = handle_print(format, &j, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-		format++;
-	}
-	va_end(osarg);
-	return (sum);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
+
+/**
+ * print_buffer - Prints the contents of the buffer
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
---
+++ pritf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format.
+ * @format: character string containing directives.
+ * Return: number of characters printed (excluding null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	int i = 0;
+	va_list args;
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+	{
+		if (format[i + 1] == '\0')
+	return (-1);
+	i++;
+		while (format[i] == ' ')
+		i++;
+	if (format[i] == '%')
+	{
+	putchar('%');
+	count++;
+	}
+	else if (format[i] == 'c')
+	count += print_char(args);
+	else if (format[i] == 's')
+	count += print_string(args);
+	else
+	return (-1);
+	}
+	else
+	{
+	putchar(format[i]);
+	count++;
+	}
+	i++;
+	}
+	va_end(args);
+	return (count);
+}
